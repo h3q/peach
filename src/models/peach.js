e469ee36c0e422960a5d543a27f66c056b5a7d03
qron <qron.144@gmail.com>
2018-03-25 13:54:53 +0200

    Refactor models

--- src/models/peach.js
+++ src/models/peach.js
@@ -1,42 +1,40 @@
-import { store } from "../firebaseService";
+import { store } from '../firebaseService';
 
-const collection = store.collection("users");
+const collection = store.collection('peaches');
 
 export class Peach {
-  constructor({ title, description, likes, creationDate, links }) {
-    Object.assign(this, {
-      title,
-      description,
-      likes,
-      creationDate,
-      links
-    });
-  }
-  update() {
-    const { uid, title, description, likes, creationDate, links } = this;
-    return collection
-      .doc(uid)
-      .set({ title, description, likes, creationDate, links }, { merge: true });
-  }
-  remove() {
-    return collection.doc(this.uid).delete();
-  }
-  save() {
-    const { title, description, likes, creationDate } = this;
-    return collection
-      .add({ title, description, likes, creationDate })
-      .then(({ id }) => !this.uid && Object.assign(this, { uid: id }));
-  }
+	constructor({ title = '', description = '', likes = [], links = [] }) {
+		Object.assign(this, {
+			title,
+			description,
+			likes,
+			links,
+			created: Date.now()
+		});
+		collection
+			.add({ ...this })
+			.then(({ id }) => Object.assign(this, { uid: id }));
+	}
+	delete() {
+		return collection.doc(this.uid).delete();
+	}
+	save() {
+		if (!this.uid) console.error('not ready yet #DEBUG');
+		const { uid, title, description, likes, links, created } = this;
+		return collection
+			.doc(uid)
+			.set({ title, description, likes, links, created });
+	}
 }
 
 export const findAll = () => {
-  return collection.get().then(peachesSnapshot => {
-    const peaches = [];
-    peachesSnapshot.forEach(doc =>
-      peaches.push(new Peach({ ...doc.data(), uid: doc.id }))
-    );
-    return peaches;
-  });
+	return collection.get().then(peachesSnapshot => {
+		const peaches = [];
+		peachesSnapshot.forEach(doc =>
+			peaches.push(new Peach({ ...doc.data(), uid: doc.id }))
+		);
+		return peaches;
+	});
 };
 
 export default Peach;
