67834a23ba48ebd84f46b19a94c1b88ebe44ae78
EmmSolom <solom.emmanuel@gmail.com>
2018-04-01 00:30:03 +0200

    Add edition mode

--- src/components/PeachCreator.js
+++ src/components/PeachCreator.js
@@ -7,68 +7,67 @@ import FlatButton from 'material-ui/FlatButton';
 import Peach from '../models/Peach';
 
 class PeachCreator extends Component {
-	constructor() {
-		super();
-		this.state = {
-			title: '',
-			description: '',
-			links: []
-		};
-	}
-	getActions = () => [
-		<FlatButton label="Cancel" secondary onClick={this.props.close} />,
-		<FlatButton label="Submit" secondary onClick={this.onSubmit} />
-	];
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      description: '',
+      links: []
+    };
+  }
+  getActions = () => [
+    <FlatButton label="Cancel" secondary onClick={this.props.close} />,
+    <FlatButton label="Submit" secondary onClick={this.onSubmit} />
+  ];
 
-	onSubmit = () => {
-		const { onSubmit, close, user } = this.props;
-		const peach = new Peach({ ...this.state, author: user.doc });
-		peach
-			.save()
-			.then(() => {
-				onSubmit(peach);
-				close();
-			})
-			.catch(error => console.error(error));
-	};
+  onSubmit = () => {
+    const { onSubmit, close, user } = this.props;
+    const peach = new Peach({ ...this.state, author: user.doc });
+    peach
+      .save()
+      .then(() => {
+        onSubmit(peach);
+        close();
+      })
+      .catch(error => console.error(error));
+  };
 
-	onTitleChange = event => this.setState({ title: event.target.value });
-	onDescriptionChange = event =>
-		this.setState({ description: event.target.value });
+  onTitleChange = event => this.setState({ title: event.target.value });
+  onDescriptionChange = event =>
+    this.setState({ description: event.target.value });
 
-	render() {
-		const { onSubmit, peach, close } = this.props;
-		return (
-			<Dialog
-				title="Format you Peach, fella."
-				actions={this.getActions()}
-				modal={false}
-				open
-				autoScrollBodyContent={true}
-				onRequestClose={close}
-			>
-				What are you going to show to those lovely morons of yours?
-				<div style={{ display: 'flex', flexDirection: 'column' }}>
-					<TextField
-						onChange={this.onTitleChange}
-						hintText="Peach title"
-						underlineShow={true}
-					/>
-					<TextField
-						onChange={this.onDescriptionChange}
-						hintText="Peach description"
-						underlineShow={true}
-						multiLine={true}
-						rows={2}
-						rowsMax={5}
-					/>
-					{/*
+  render() {
+    const { onSubmit, peach, close } = this.props;
+    return (
+      <Dialog
+        title="Format you Peach, fella."
+        actions={this.getActions()}
+        modal={false}
+        open
+        autoScrollBodyContent={true}
+        onRequestClose={close}>
+        What are you going to show to those lovely morons of yours?
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <TextField
+            onChange={this.onTitleChange}
+            hintText="Peach title"
+            underlineShow={true}
+          />
+          <TextField
+            onChange={this.onDescriptionChange}
+            hintText="Peach description"
+            underlineShow={true}
+            multiLine={true}
+            rows={1}
+            rowsMax={5}
+          />
+          {/*
             * TODO: add links picker
             */}
-				</div>
-			</Dialog>
-		);
-	}
+        </div>
+      </Dialog>
+    );
+  }
 }
 
 export default PeachCreator;
--- src/components/PeachList.js
+++ src/components/PeachList.js
@@ -15,84 +15,81 @@ import FlatButton from 'material-ui/FlatButton';
 import { Toolbar, ToolbarGroup } from 'material-ui/Toolbar';
 
 const styles = {
-	root: {
-		display: 'flex',
-		alignItems: 'flex-start',
-		flexWrap: 'wrap'
-	}
+  root: {
+    display: 'flex',
+    alignItems: 'flex-start',
+    flexWrap: 'wrap'
+  }
 };
 
-// TODO: add peach'likes to css rule "flex-order" (?), in order to sort peaches by descending likess
-
 export class PeachList extends Component {
-	constructor() {
-		super();
-		this.state = {
-			open: false,
-			peaches: []
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      open: false,
+      peaches: []
+    };
+  }
 
-	handleOpen = () => {
-		this.setState({ open: true });
-	};
-	handleClose = () => {
-		this.setState({ open: false });
-	};
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+  handleClose = () => {
+    this.setState({ open: false });
+  };
 
-	componentWillMount = () => {
-		fetchAllPeach().then(peaches => {
-			this.setState({ peaches: peaches });
-		});
-	};
+  componentWillMount = () => {
+    fetchAllPeach().then(peaches => {
+      this.setState({ peaches: peaches });
+    });
+  };
 
-	addPeach = peach =>
-		this.setState(({ peaches }) => ({ peaches: [...peaches, peach] }));
+  addPeach = peach =>
+    this.setState(({ peaches }) => ({ peaches: [...peaches, peach] }));
 
-	removePeach = removed =>
-		this.setState(({ peaches }) => ({
-			peaches: peaches.filter((peach, index) => index !== removed)
-		}));
+  removePeach = removed =>
+    this.setState(({ peaches }) => ({
+      peaches: peaches.filter((peach, index) => index !== removed)
+    }));
 
-	render() {
-		const { user } = this.props;
-		const { peaches } = this.state;
+  render() {
+    const { user } = this.props;
+    const { peaches } = this.state;
 
-		return (
-			<div>
-				<Toolbar style={{ justifyContent: 'flex-end' }}>
-					<ToolbarGroup>
-						<FloatingActionButton
-							onClick={this.handleOpen}
-							secondary
-							style={{ position: 'relative', top: '50%' }}
-						>
-							<ContentAdd />
-						</FloatingActionButton>
-					</ToolbarGroup>
-				</Toolbar>
-				<List className="peaches">
-					{peaches.map((peach, index) => (
-						<PeachItem
-							peach={peach}
-							index={index}
-							key={index}
-							user={user}
-							remove={this.removePeach}
-						/>
-					))}
-				</List>
+    return (
+      <div>
+        <Toolbar style={{ justifyContent: 'flex-end' }}>
+          <ToolbarGroup>
+            <FloatingActionButton
+              onClick={this.handleOpen}
+              secondary
+              style={{ position: 'relative', top: '50%' }}>
+              <ContentAdd />
+            </FloatingActionButton>
+          </ToolbarGroup>
+        </Toolbar>
+        <List className="peaches">
+          {peaches.map((peach, index) => (
+            <PeachItem
+              peach={peach}
+              index={index}
+              key={index}
+              user={user}
+              remove={this.removePeach}
+            />
+          ))}
+        </List>
 
-				{this.state.open && (
-					<PeachCreator
-						onSubmit={this.addPeach}
-						close={this.handleClose}
-						user={user}
-					/>
-				)}
-			</div>
-		);
-	}
+        {this.state.open && (
+          <PeachCreator
+            onSubmit={this.addPeach}
+            close={this.handleClose}
+            user={user}
+          />
+        )}
+      </div>
+    );
+  }
 }
 
 export default PeachList;
